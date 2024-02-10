dad5e6feb1203f4761813a2a11a569bef3209a44
PranayTejaReddy27 <pranayteja270@gmail.com>
2024-02-10 21:36:54 +0530

    firstMain

---
+++ aitrainer.py
@@ -0,0 +1,12 @@
+import cv2
+import pandas as pd
+import time
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    #img = cv2.resize(img, (1280,720))
+    cv2.imshow("Image",img)
+    cv2.waitKey(1)
+    
