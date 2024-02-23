9e6bad6f1211b41d0eb2f157e919bbb3a0366c8f
PranayTejaReddy27 <pranayteja270@gmail.com>
2024-02-23 15:29:07 +0530

    firstMain

--- aitrainer.py
+++ aitrainer.py
@@ -1,15 +1,16 @@
 import cv2
-#import pandas as pd
+import pandas as pd
 import numpy as np
 import time
 import PoseModule as pm
 
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('Videos/dumbbells-5.mov')
 detector = pm.poseDetector()
 count = 0
 dir  = 0
 pTime = 0
+df = pd.DataFrame(columns=['position', '11x', '11y', 'angle', 'percentage','polarity'])
 
 while True:
     success, img = cap.read()
@@ -20,16 +21,14 @@ while True:
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
-    # display FPS
-    #cv2.putText(img, str(int(fps)), (70,50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
-    #print(lmList)
+    #print(lmList[])
     if len(lmList) != 0: 
         # left arm 
         angle = detector.findAngle(img, 11, 13, 15)
         # right arm
         #detector.findAngle(img, 12, 14, 16)
-        per = np.interp(angle,(200,320),(0,100))
-        bar = np.interp(angle,(200,320),(450,100))
+        per = np.interp(angle,(250,300),(0,100))
+        bar = np.interp(angle,(250,300),(450,100))
         #print(angle,per)
         if per == 100 :
             if dir == 0:
@@ -40,7 +39,12 @@ while True:
             if dir == 1:
                 count += 0.5
                 dir = 0
-        print(count)
+
+        # Append new row of data to the DataFrame
+        new_data = {'position': "11,13,15", '11x': lmList[11][1], '11y': lmList[11][2], 'angle': angle, 'percentage': per, 'polarity': 1}
+        df = df._append(new_data, ignore_index=True)
+        
+        #print(count)
         # accuracy bar 
         cv2.rectangle(img, (540,100),(580,450),(123,232,121),2)
         cv2.rectangle(img, (540,int(bar)),(580,450),(123,232,121),cv2.FILLED)
@@ -49,14 +53,16 @@ while True:
         cv2.rectangle(img, (20,10),(200,100),(123,232,121),cv2.FILLED)
         # count text display
         cv2.putText(img, str(int(count)), (90,90), cv2.FONT_HERSHEY_SIMPLEX,2, (23,34,10),3)
-        # display FPS
-    cv2.putText(img,"FPS : " + str(int(fps)), (70,30), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 2)
 
+    # display FPS
+    cv2.putText(img,"FPS : " + str(int(fps)), (70,30), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 2)
     cv2.imshow("Image",img)
     key = cv2.waitKey(1)
     if key == ord('q'):
         break  # Exit the loop if 'q' is pressed
-
+print(df)
+df.to_csv('Dumbbells_DataSet4.csv', index=False, encoding='utf-8' )
 img.release()  # Release the camera
 cv2.destroyAllWindows()
+
     
